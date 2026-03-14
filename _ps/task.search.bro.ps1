1b6255fa5f45ebd1b735331103197b6371b687a4
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2026-03-15 01:41:21 +0300

    PS: Search Task by name part

---
+++ _ps/task.search.bro.ps1
@@ -0,0 +1,34 @@
+$searchTerm = "org"
+$matchingTasks = @()
+
+Get-ScheduledTask | ForEach-Object {
+    $task = $_
+    $nameMatch = $task.TaskName -like "*$searchTerm*"
+    $descriptionMatch = $false
+    $commandMatch = $false
+
+    # Check description (if available)
+    if ($task.Description) {
+        $descriptionMatch = $task.Description -like "*$searchTerm*"
+    }
+
+    # Check command/action
+    $task.Actions | ForEach-Object {
+        if ($_.Execute -like "*$searchTerm*" -or $_.Arguments -like "*$searchTerm*") {
+            $commandMatch = $true
+        }
+    }
+
+    if ($nameMatch -or $descriptionMatch -or $commandMatch) {
+        $matchingTasks += $task
+    }
+}
+
+# Output results
+$matchingTasks | Select-Object TaskName, TaskPath, State, Description, @{
+    Name = "Command";
+    Expression = { ($_.Actions.Execute -join "; ") }
+}, @{
+    Name = "Arguments";
+    Expression = { ($_.Actions.Arguments -join "; ") }
+} | Format-Table -AutoSize
