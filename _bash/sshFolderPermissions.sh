72f93b07f9ceeda237a0ef1d61f25ee02487c939
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2026-03-15 12:02:27 +0300

    dev

--- _bash/sshFolderPermissions.sh
+++ _bash/sshFolderPermissions.sh
@@ -1,5 +1,6 @@
 sshFolderPermissions() {
     local ssh_dir="$HOME/.ssh"
+    local known_hosts="$ssh_dir/known_hosts"
 
     if [[ ! -d "$ssh_dir" ]]; then
         echo "Ошибка: каталог $ssh_dir не существует." >&2
@@ -7,14 +8,22 @@ sshFolderPermissions() {
     fi
 
     chmod 700 "$ssh_dir"
-    echo "700 $ssh_dir"
+    echo "Установлены права 700 для каталога: $ssh_dir"
 
     find "$ssh_dir" -maxdepth 1 -type f ! -name "*.*" -exec chmod 600 {} \;
-    echo "600 *.*"
+    echo "Установлены права 600 для файлов без расширения в: $ssh_dir"
 
     find "$ssh_dir" -maxdepth 1 -type f -name "*.private" -exec chmod 600 {} \;
-    echo "600 *.private"
+    echo "Установлены права 600 для файлов с расширением .private в: $ssh_dir"
 
     find "$ssh_dir" -maxdepth 1 -type f -name "*.pub" -exec chmod 644 {} \;
-    echo "644 *.pub"
+    echo "Установлены права 644 для файлов с расширением .pub в: $ssh_dir"
+
+    # Обработка файла known_hosts
+    if [[ -f "$known_hosts" ]]; then
+        chmod 644 "$known_hosts"
+        echo "Установлены права 644 для файла: $known_hosts"
+    else
+        echo "Файл $known_hosts не найден, пропускаем установку прав."
+    fi
 }
