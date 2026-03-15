31c6096919765d384976920e709dedc9c44bc5f9
VsevolodAzovsky <vsevolod.azovsky@gmail.com>
2026-03-15 12:02:39 +0300

    dev

--- _bash/sshFolderPermissions.sh
+++ _bash/sshFolderPermissions.sh
@@ -1,6 +1,7 @@
 sshFolderPermissions() {
     local ssh_dir="$HOME/.ssh"
     local known_hosts="$ssh_dir/known_hosts"
+    local config_file="$ssh_dir/config"
 
     if [[ ! -d "$ssh_dir" ]]; then
         echo "Ошибка: каталог $ssh_dir не существует." >&2
@@ -25,5 +26,13 @@ sshFolderPermissions() {
         echo "Установлены права 644 для файла: $known_hosts"
     else
         echo "Файл $known_hosts не найден, пропускаем установку прав."
+    fi
+
+    # Обработка файла config
+    if [[ -f "$config_file" ]]; then
+        chmod 600 "$config_file"
+        echo "Установлены права 600 для файла: $config_file"
+    else
+        echo "Файл $config_file не найден, пропускаем установку прав."
     fi
 }
