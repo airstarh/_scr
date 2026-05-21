a976bf8b4bb5806676bf3e3320e04df8cbe9ca98
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-05-21 03:59:21 +0300

    dev

--- _dot/linux/_bash/ln.sh
+++ _dot/linux/_bash/ln.sh
@@ -1,4 +1,17 @@
+#!/usr/bin/bash
+
 ln -s ~/_A001/_scr/_GITOUT/000 ~/000
 ln -s ~/_A001/_scr/_symlinks/ln-exe ~/ln-exe
 ln -s ~/_A001/_scr/_symlinks/ln-log ~/ln-log
 ln -s ~/_A001/_scr/_symlinks/ln-draft ~/ln-draft
+
+ln -s ~/_A001/_scr/_symlinks/000.sh ~/000
+ln -s ~/_A001/_scr/_symlinks/010.sh ~/010
+ln -s ~/_A001/_scr/_symlinks/020.sh ~/020
+ln -s ~/_A001/_scr/_symlinks/030.sh ~/030
+ln -s ~/_A001/_scr/_symlinks/040.sh ~/040
+ln -s ~/_A001/_scr/_symlinks/050.sh ~/050
+ln -s ~/_A001/_scr/_symlinks/060.sh ~/060
+ln -s ~/_A001/_scr/_symlinks/070.sh ~/070
+ln -s ~/_A001/_scr/_symlinks/080.sh ~/080
+ln -s ~/_A001/_scr/_symlinks/090.sh ~/090
