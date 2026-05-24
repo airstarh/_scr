483678268355c7eeb5d792d260a48245cc6db4dd
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-05-24 21:36:18 +0300

    dev

--- _symlinks/010.sh
+++ _symlinks/010.sh
@@ -6,8 +6,8 @@ qemu-system-x86_64 \
     -drive file=/home/qqq/qemu/w7x64_HDA.img,format=raw,if=ide \
     -cdrom /dev/null \
     -boot c \
-    -net user,smb=/home/qqq/shared_qemu/ \
-    -net nic,model=virtio \
+    -netdev user,id=net0,smb=/home/qqq/shared_qemu/ \
+    -device e1000,netdev=net0 \
     -vga qxl \
     -device AC97 \
     -usb \
