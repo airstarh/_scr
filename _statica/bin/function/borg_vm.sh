c95a0f53d02678108d246d2cae345982642c4dba
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-05-24 22:58:01 +0300

    dev

--- _statica/bin/function/borg_vm.sh
+++ _statica/bin/function/borg_vm.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#! /bin/bash
+
+borg_vm() {
 qemu-system-x86_64 \
     -enable-kvm \
     -m 4096 \
@@ -12,9 +14,5 @@ qemu-system-x86_64 \
     -device AC97 \
     -usb \
     -device usb-tablet \
-    -monitor stdio \
-
-    # Новые строки для буфера обмена
-    -chardev qemu-vdagent,id=vdagent,name=vdagent,clipboard=on \
-    -device virtio-serial-pci \
-    -device virtserialport,chardev=vdagent,name=com.redhat.spice.0
+    -monitor stdio
+}
