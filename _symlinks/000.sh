9b0c6345d6bf683efcc7d60fe5252bd237c0fb11
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-06-11 14:19:03 +0300

    dev

--- _symlinks/000.sh
+++ _symlinks/000.sh
@@ -1,6 +1,1 @@
-# Check MySQL error log
-docker logs alina_mysql 2>&1 | grep -i error
-
-# Test query performance
-docker exec alina_mysql mysql -e "SHOW VARIABLES LIKE 'tmpdir';"
-docker exec alina_mysql mysql -e "SHOW VARIABLES LIKE 'innodb_temp_data_file_path';"
+find . -type d -name "*" | cut -d'/' -f2-
