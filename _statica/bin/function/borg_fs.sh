2129528e426ccc67dd0672890286c1c63a9859fd
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-08-20 03:46:03 +0300

    dev

--- _statica/bin/function/borg_fs.sh
+++ _statica/bin/function/borg_fs.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-borg_fs_A001(){
+bash_fs_rsync() {
+    local source_dir="$1"
+    local target_dir="$2"
+
     rsync -rtlvz --delete --no-perms --no-owner --no-group --progress \
         -i \
         --modify-window=10 \
-        /home/qqq/_A001/ \
-        qqq@192.168.1.120:/home/qqq/_A001/
+        "$source_dir" \
+        "$target_dir"
+}
+
+
+borg_fs_sync(){
+    local STARTED=$EPOCHREALTIME
+    ##################################################
+    borg_fs_A001
+    borg_fs_v
+    ##################################################
+    local ENDED=$EPOCHREALTIME
+    borg_spent $STARTED $ENDED
+}
+borg_fs_A001(){
+    bash_fs_rsync "/home/qqq/_A001/" "qqq@192.168.1.120:/home/qqq/_A001/"
 }
 
 borg_fs_v(){
-    local STARTED=$EPOCHREALTIME
-
-    # REMOVED --ignore-times so rsync can skip unchanged files
-    rsync -rtlvz --delete --no-perms --no-owner --no-group --progress \
-        -i \
-        --modify-window=10 \
-        /mnt/d1001/_v/ \
-        qqq@192.168.1.120:/mnt/d1001/_v/
-
-    local ENDED=$EPOCHREALTIME
-    borg_spent $STARTED $ENDED
+    bash_fs_rsync "/mnt/d1001/_v/" "qqq@192.168.1.120:/mnt/d1001/_v/"
 }
 
 
