81e0361881197599825f74513774d799cd06d2a7
Vsevolod Azovsky <vsevolod.azovsky@gmail.com>
2026-08-21 03:43:23 +0300

    dev

--- _dot/tech/docker/move.to.sys.sh
+++ _dot/tech/docker/move.to.sys.sh
@@ -1,26 +1,29 @@
 #! /bin/bash
 
-NEW__PATH_DOCKER="/mnt/d1001/dockersys/docker"
-NEW__PATH_CONTAINERD="/mnt/d1001/dockersys/containerd"
+NEW_PATH_DOCKER="/mnt/d1001/dockersys/docker"
+NEW_PATH_CONTAINERD="/mnt/d1001/dockersys/containerd"
 
 sudo systemctl stop docker.socket docker containerd
 
-sudo mkdir -p "$NEW__PATH_DOCKER"
-sudo mkdir -p "$NEW__PATH_CONTAINERD"
+sudo mkdir -p "$NEW_PATH_DOCKER"
+sudo mkdir -p "$NEW_PATH_CONTAINERD"
 
 ### Paths:
 ### dockersys
 ###     docker (/var/lib/docker)
-sudo rsync -aHAX --info=progress2 /var/lib/docker/ "$NEW__PATH_DOCKER/"
+sudo rsync -aHAX --info=progress2 /var/lib/docker/ "$NEW_PATH_DOCKER/"
+sudo mv /var/lib/docker/* "$NEW_PATH_DOCKER/"
 
 ###     containerd (/var/lib/containerd)
-sudo rsync -aHAX --info=progress2 /var/lib/containerd/ "$NEW__PATH_CONTAINERD/"
+sudo rsync -aHAX --info=progress2 /var/lib/containerd/ "$NEW_PATH_CONTAINERD/"
+sudo mv /var/lib/containerd/* "$NEW_PATH_CONTAINERD/"
+
 ###
 ### Configs:
 ### /etc/docker/daemon.json
-###     "data-root": "$NEW__PATH_DOCKER"
+###     "data-root": "$NEW_PATH_DOCKER"
 ### /etc/containerd/config.toml
-###     root = "$NEW__PATH_CONTAINERD"
+###     root = "$NEW_PATH_CONTAINERD"
 ###
 ##################################################
 
